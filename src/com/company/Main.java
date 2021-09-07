abaa5065a626736e1623e9137d313d83e47ee53d
Timhansen1990 <timx1776@edu.easj.dk>
2021-09-07 11:44:56 +0200

    Time.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("hello world");
+        int hour = 11;
+        int minute = 35;
+        int seconds = 12;
+
+        System.out.print("Seconds since midnight: ");
+        System.out.println((hour * 60 * 60) + minute * 60 + seconds );
+
+        System.out.print("How many seconds is remaining of the day:");
+        System.out.print(86400 - ((hour * 60 * 60) + minute * 60 + seconds));
+
+
     }
 }
